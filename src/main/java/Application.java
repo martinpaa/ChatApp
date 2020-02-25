862c71f5cabfabcbdc385ce723dd62ba0e8660a9
martinpa <ana.martin-pero@roche.com>
2020-02-25 15:46:38 +0100

    first classes

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,7 +2,14 @@ import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
+        String input,output = "";
         Scanner userInput = new Scanner(System.in);
+        while(true){
+            if (userInput.hasNext()){
+                input = userInput.nextLine();
+                 //CommandProcessor.execute(input);
+            }
+        }
 
     }
 }
