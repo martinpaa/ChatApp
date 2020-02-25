bf29bc7fb1deb93e4882fbe5f35c22128d79c716
martinpa <ana.martin-pero@roche.com>
2020-02-25 15:51:27 +0100

    first test

---
+++ src/test/java/MessageControllerShould.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class MessageControllerShould {
+
+    @Test
+    public  void register_comment_when_user_talks(){
+
+        MessageController savePost = new MessageController();
+    }
+}
